c25d5c69bcae6209bd29ca5c3db03364b17d5376
abr2208 <115437169+abr2208@users.noreply.github.com>
2022-10-09 22:53:26 -0300

    Add files via upload

---
+++ Adalberto.php
@@ -0,0 +1,98 @@
+ <?php  
+
+$carro = array ( 
+
+"Carro" => "lamborguini" 
+"Azul" => true); 
+
+echo $array["carro"];
+
+
+
+foreach($carro as $carro){ 
+
+if ($carro["Azul"]==true){
+
+$carro["Carro"] == $carro['Azul'];
+
+echo $carro['Azul']
+
+}else{ 
+
+$carro ["Azul"] == true; 
+
+
+}
+}
+
+?>
+)
+
+
+}
+
+?>
+)
+[22:17, 09/10/2022] Jediael(META): <?php  
+
+$carro = array ( 
+
+"Carro" => "lamborguini" 
+"Preto" => true); 
+
+echo $array["carro"];
+
+
+
+foreach($carro as $carro){ 
+
+if ($carro["Preto"]==true){
+
+$carro["Carro"] == $carro['Preto'];
+
+echo $carro['Preto']
+
+}else{ 
+
+$carro ["Preto"] == true; 
+
+
+}
+}
+
+?>
+)
+
+
+}
+
+?>
+)<?php  
+
+$carro = array ( 
+
+"Carro" => "lamborguini" 
+"Verde" => true); 
+
+echo $array["carro"];
+
+
+
+foreach($carro as $carro){ 
+
+if ($carro["Verde"]==true){
+
+$carro["Carro"] == $carro['Verde'];
+
+echo $carro['Verde']
+
+}else{ 
+
+$carro ["Verde"] == true; 
+
+
+}
+}
+
+?>
+)
